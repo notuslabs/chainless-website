eff7d572ab75605def7258ee77fff9e7ee3082d6
Kevin Voigt <kevin.voigt@notus.team>
2026-04-07 13:03:28 -0300

    normalize store button label font size to text-xs across hero and influencer pages

--- landing/src/components/influencer-page.tsx
+++ landing/src/components/influencer-page.tsx
@@ -150,7 +150,7 @@ export function InfluencerPage({ name, code }: InfluencerPageProps) {
                         <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.8-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z"/>
                       </svg>
                       <div className="flex flex-col">
-                        <span className="whitespace-nowrap text-[10px] font-medium leading-none text-white/50">Disponível na</span>
+                        <span className="whitespace-nowrap text-xs font-medium leading-none text-white/50">Disponível na</span>
                         <span className="whitespace-nowrap text-sm font-semibold leading-tight text-text-primary/80">App Store</span>
                       </div>
                     </a>
@@ -173,7 +173,7 @@ export function InfluencerPage({ name, code }: InfluencerPageProps) {
                         <path d="M3.61 1.814L13.793 12 3.61 22.186a.996.996 0 0 1-.61-.92V2.734a1 1 0 0 1 .61-.92zm.46-.374L14.5 7.5l-2.5 2.5L4.07 1.44zM14.5 16.5L4.07 22.56 12 14.5l2.5 2zm.5-.5l5.4-3.06c.36-.2.6-.56.6-.94s-.24-.74-.6-.94L15 8l-3 4 3 4z"/>
                       </svg>
                       <div className="flex flex-col">
-                        <span className="whitespace-nowrap text-[10px] font-medium leading-none text-white/50">Disponível no</span>
+                        <span className="whitespace-nowrap text-xs font-medium leading-none text-white/50">Disponível no</span>
                         <span className="whitespace-nowrap text-sm font-semibold leading-tight text-text-primary/80">Google Play</span>
                       </div>
                     </a>
